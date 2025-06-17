8574fd79293ac72dc4e2aaf6d7135432bcf20dbc
Jose de Lima <delimajm@gmail.com>
2025-06-16 23:29:31 -0500

    fix package.json

--- resume/settings.py
+++ resume/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
 
